537b31887a1c43efbf29ee6b2e2d8c4c8a35d9b3
Breno Roosevelt <breno.jesus@ufms.br>
2021-08-12 14:37:05 -0400

    initial commit

--- src/LeagueRouterAttributeProvider.php
+++ src/LeagueRouterAttributeProvider.php
@@ -6,13 +6,21 @@ namespace BrenoRoosevelt\RouteAttributeProvider\League;
 use Jerowork\RouteAttributeProvider\Api\Route;
 use Jerowork\RouteAttributeProvider\RouteAttributeConfigurator;
 use Jerowork\RouteAttributeProvider\RouteAttributeProviderInterface;
+use League\Route\ContainerAwareInterface;
+use League\Route\ContainerAwareTrait;
 use League\Route\Router;
+use Psr\Container\ContainerInterface;
 use Psr\SimpleCache\CacheInterface;
 
-class LeagueRouterAttributeProvider implements RouteAttributeProviderInterface
+class LeagueRouterAttributeProvider implements RouteAttributeProviderInterface, ContainerAwareInterface
 {
-    final public function __construct(private Router $router)
+    use ContainerAwareTrait;
+
+    final public function __construct(private Router $router, ?ContainerInterface $container = null)
     {
+        if ($container instanceof ContainerInterface) {
+            $this->setContainer($container);
+        }
     }
 
     /**
@@ -22,26 +30,82 @@ class LeagueRouterAttributeProvider implements RouteAttributeProviderInterface
     {
         foreach ($route->getMethods() as $httpMethod) {
             $leagueRoute = $this->router->map($httpMethod, $route->getPattern(), [$className, $methodName]);
+
             if (null !== ($name = $route->getName())) {
                 $leagueRoute->setName($name);
             }
 
-            foreach ($route->getMiddleware() as $middleware) {
-                $leagueRoute->lazyMiddleware($middleware);
+            if (null !== ($host = $route->getHost())) {
+                $leagueRoute->setHost($host);
             }
+
+            $this->parsePorts($leagueRoute, $route);
+            $this->parseSchemes($leagueRoute, $route);
+            $this->parseStrategy($leagueRoute, $route);
+            $this->parseMiddlewares($leagueRoute, $route);
+        }
+    }
+
+    private function parsePorts(\League\Route\Route $leagueRoute, Route $route): void
+    {
+        $httpPort = $route->getHttpPort();
+        if (null !== $httpPort && in_array('http', $route->getSchemes())) {
+            $leagueRoute->setPort($httpPort);
+        }
+
+        $httpsPort = $route->getHttpsPort();
+        if (null !== $httpsPort && in_array('https', $route->getSchemes())) {
+            $leagueRoute->setPort($httpsPort);
+        }
+    }
+
+    private function parseStrategy(\League\Route\Route $leagueRoute, Route $route): void
+    {
+        $strategy = $route->getOptions()['strategy'] ?? null;
+        $container = $this->getContainer();
+        if (is_string($strategy) &&
+            $container instanceof ContainerInterface &&
+            $container->has($strategy)
+        ) {
+            $strategyInstance = $container->get($strategy);
+            if ($strategyInstance instanceof ContainerAwareInterface) {
+                $strategyInstance->setContainer($container);
+            }
+
+            $leagueRoute->setStrategy($strategyInstance);
+        }
+    }
+
+    private function parseMiddlewares(\League\Route\Route $leagueRoute, Route $route): void
+    {
+        foreach ($route->getMiddleware() as $middleware) {
+            $leagueRoute->lazyMiddleware($middleware);
+        }
+    }
+
+    private function parseSchemes(\League\Route\Route $leagueRoute, Route $route): void
+    {
+        foreach ($route->getSchemes() as $scheme) {
+            $leagueRoute->setScheme($scheme);
         }
     }
 
     /**
-     * Helper to create and apply routes with default configuration
+     * Helper to apply routes with the default configuration
      *
      * @param Router $router
      * @param array $directories
      * @param CacheInterface|null $cache
+     * @param ContainerInterface|null $container
      */
-    public static function apply(Router $router, array $directories, ?CacheInterface $cache = null): void
+    public static function apply(
+        Router $router,
+        array $directories,
+        ?CacheInterface $cache = null,
+        ?ContainerInterface $container = null
+    ): void
     {
-        $configurator = new RouteAttributeConfigurator(new self($router));
+        $configurator = new RouteAttributeConfigurator(new self($router, $container));
         $configurator->addDirectory(...$directories);
         if ($cache instanceof CacheInterface) {
             $configurator->enableCache($cache);
--- tests/LeagueRouterAttributeProviderTest.php
+++ tests/LeagueRouterAttributeProviderTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 namespace BrenoRoosevelt\RouteAttributeProvider\League\Tests;
 
 use BrenoRoosevelt\RouteAttributeProvider\League\LeagueRouterAttributeProvider;
+use Habemus\Container;
 use Jerowork\RouteAttributeProvider\Api\Route;
 use League\Route\Router;
+use League\Route\Strategy\ApplicationStrategy;
+use League\Route\Strategy\StrategyInterface;
 use PHPUnit\Framework\TestCase;
 use stdClass;
 
@@ -16,8 +19,19 @@ class LeagueRouterAttributeProviderTest extends TestCase
     {
         // arrange
         $router = new Router();
-        $route = new Route('/home', ['GET'], 'routeName', ['Middleware1', 'Middleware2']);
-        $provider = new LeagueRouterAttributeProvider($router);
+        $route = new Route(
+            '/home',
+            ['GET'],
+            'routeName',
+            ['Middleware1', 'Middleware2'],
+            'localhost',
+            ['http', 'https'],
+            80,
+            443,
+            ['strategy' => ApplicationStrategy::class]
+        );
+
+        $provider = new LeagueRouterAttributeProvider($router, new Container());
 
         // act
         $provider->configure(stdClass::class, '__invoke', $route);
@@ -27,6 +41,10 @@ class LeagueRouterAttributeProviderTest extends TestCase
         $this->assertEquals('routeName', $leagueRoute->getName());
         $this->assertEquals('GET', $leagueRoute->getMethod());
         $this->assertEquals('/home', $leagueRoute->getPath());
+        $this->assertEquals('localhost', $leagueRoute->getHost());
+        $this->assertEquals('https', $leagueRoute->getScheme());
+        $this->assertEquals(443, $leagueRoute->getPort());
+        $this->assertInstanceOf(StrategyInterface::class, $leagueRoute->getStrategy());
         foreach ($leagueRoute->getMiddlewareStack() as $middleware) {
             $this->assertTrue(in_array($middleware, ['Middleware1', 'Middleware2']));
         }
